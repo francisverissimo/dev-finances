714e21c293eda768eaed8c171382a47dded32f89
Francis Verissimo <francissv97@gmail.com>
2022-11-06 00:41:06 -0400

    add new type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,16 @@
 import { Timestamp } from "firebase/firestore";
+import { Moment } from "moment";
 
 export interface Transaction {
   id: string;
   description: string;
   value: number;
   date: Timestamp;
+}
+
+export interface AddTransactionFormFieldValues {
+  description: string;
+  type: "expense" | "income";
+  value: number;
+  date: Moment;
 }
