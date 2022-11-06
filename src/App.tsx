fe235a6429139352dea12ee5aaa732031e3c419c
Francis Verissimo <francissv97@gmail.com>
2022-11-06 00:45:46 -0400

    improve calculation of numbers, organize a little

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import { useEffect, useState } from "react";
-import { doc, getDoc, onSnapshot, Timestamp } from "firebase/firestore";
+import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "./services/firebase";
-import {
-  ArrowCircleDown,
-  ArrowCircleUp,
-  CurrencyDollar,
-  Plus,
-} from "phosphor-react";
+import { Transaction } from "./types";
 import { Header } from "./components/Header";
 import { ModalAddTransaction } from "./components/ModalAddTransaction";
 import { TransactionCard } from "./components/TransactionCard";
-import { Transaction } from "./types";
+import { Numbers } from "./components/Numbers";
+import { Footer } from "./components/Footer";
+import { CircleNotch, Plus } from "phosphor-react";
 
 export function App() {
   const [openModalAddTransaction, setOpenModalAddTransaction] = useState(false);
   const [transactions, setTransactios] = useState<Transaction[]>();
   const [incomes, setIncomes] = useState<number>(0);
   const [expenses, setExpenses] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   function handleOpenModalAddTransaction() {
     setOpenModalAddTransaction(true);
@@ -30,36 +28,29 @@ export function App() {
     const unsub = onSnapshot(
       doc(db, "users", "francissv97@gmail.com"),
       (doc) => {
-        const data = Object.values(doc.data() as Transaction[]);
+        let incomeCount = 0;
+        let expenseCount = 0;
+
+        const data = doc.data() as { transactions: Transaction[] };
 
         setTransactios(
-          data.sort(
+          data.transactions.sort(
             (a: Transaction, b: Transaction) =>
               a.date.toMillis() - b.date.toMillis()
           )
         );
 
-        if (data) {
-          data.forEach(
-            (transaction) =>
-              transaction.value >= 0 &&
-              setIncomes(
-                incomes
-                  ? incomes + transaction.value / 100
-                  : 0 + transaction.value / 100
-              )
-          );
-
-          data.forEach(
-            (transaction) =>
-              transaction.value < 0 &&
-              setExpenses(
-                expenses
-                  ? expenses + transaction.value / 100
-                  : 0 + transaction.value / 100
-              )
-          );
+        for (let i in data.transactions) {
+          if (data.transactions[i].value >= 0) {
+            incomeCount += data.transactions[i].value / 100;
+          } else {
+            expenseCount += data.transactions[i].value / 100;
+          }
         }
+
+        setIncomes(incomeCount);
+        setExpenses(expenseCount);
+        setIsLoading(false);
       }
     );
 
@@ -67,51 +58,11 @@ export function App() {
   }, []);
 
   return (
-    <div className="bg-gradient-to-t from-slate-500 via-slate-300 to-slate-300 min-h-screen bg-no-repeat">
+    <div className="flex flex-col bg-gradient-to-t from-slate-500 via-slate-300 to-slate-300 min-h-screen bg-no-repeat">
       <Header />
 
-      <div className="max-w-xl mx-auto flex flex-col gap-4 px-4 -translate-y-6">
-        <div className="flex bg-slate-200 justify-between text-green-700 p-4 rounded-md shadow-lg">
-          <div className="flex flex-col gap-2">
-            <span className="text-zinc-600">Entradas</span>
-            <span className="text-xl font-medium">
-              {incomes.toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              })}
-            </span>
-          </div>
-
-          <ArrowCircleUp size={32} />
-        </div>
-
-        <div className="flex bg-slate-200 justify-between text-red-900 p-4 rounded-md shadow-lg">
-          <div className="flex flex-col gap-2">
-            <span className="text-zinc-600">Saídas</span>
-            <span className="text-xl font-medium">
-              {expenses.toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              })}
-            </span>
-          </div>
-
-          <ArrowCircleDown size={32} />
-        </div>
-
-        <div className="flex justify-between bg-gradient-to-r from-slate-700 to-slate-500 p-4 text-zinc-100 rounded-md shadow-lg">
-          <div className="flex flex-col gap-2">
-            <span>Total</span>
-            <span className="text-xl font-medium">
-              {(incomes + expenses).toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              })}
-            </span>
-          </div>
-
-          <CurrencyDollar size={32} />
-        </div>
+      <div className="max-w-xl flex mx-auto w-full flex-col gap-4 px-4 -translate-y-6">
+        <Numbers incomes={incomes} expenses={expenses} loading={isLoading} />
 
         <button
           onClick={handleOpenModalAddTransaction}
@@ -127,17 +78,18 @@ export function App() {
         <div className="h-[1px] my-4 bg-gradient-to-r from-slate-900 via-slate-500 to-slate-400 rounded-md"></div>
 
         <div className="flex flex-col gap-4">
-          {transactions &&
+          {isLoading ? (
+            <CircleNotch className="text-slate-500 animate-spin self-center" size={48} />
+          ) : (
+            transactions &&
             transactions.map((transaction) => (
-              <TransactionCard
-                key={transaction.id}
-                description={transaction.description}
-                date={transaction.date}
-                value={transaction.value}
-              />
-            ))}
+              <TransactionCard key={transaction.id} transaction={transaction} />
+            ))
+          )}
         </div>
       </div>
+
+      <Footer className="mt-auto" />
 
       {openModalAddTransaction && (
         <ModalAddTransaction
