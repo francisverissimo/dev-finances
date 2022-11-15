a4e1f8d9c5dbbf3a74505fb6e7f331c8ed819d4a
Francis Verissimo <francissv97@gmail.com>
2022-11-15 00:34:24 -0400

    add signout button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,21 @@
+import { SignOut } from "phosphor-react";
+import { useAuth } from "../hooks/useAuth";
+import { Logo } from "./Logo";
+
 export function Header() {
+  const { logOut } = useAuth();
+
   return (
     <div className="bg-gradient-to-tr from-slate-800 to-slate-600">
-      <div className="max-w-xl mx-auto px-4 pt-4 pb-10">
-        <strong className="text-zinc-50 text-xl font-medium">
-          DevFinance$
-        </strong>
+      <div className="flex justify-between max-w-xl mx-auto px-4 pt-4 pb-10">
+        <Logo className="text-slate-200" />
+
+        <div className="group cursor-pointer px-2" onClick={logOut}>
+          <SignOut
+            className="group transition text-red-700 group-hover:text-red-600"
+            size={32}
+          />
+        </div>
       </div>
     </div>
   );
