9adb7db0cf5591378350a54edbcb4bba1abb171b
Francis Verissimo <francissv97@gmail.com>
2022-11-15 00:33:33 -0400

    refact app, use react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,17 @@
-import { useEffect, useState } from "react";
-import { doc, onSnapshot } from "firebase/firestore";
-import { db } from "./services/firebase";
-import { Transaction } from "./types";
-import { Header } from "./components/Header";
-import { ModalAddTransaction } from "./components/ModalAddTransaction";
-import { TransactionCard } from "./components/TransactionCard";
-import { Numbers } from "./components/Numbers";
-import { Footer } from "./components/Footer";
-import { CircleNotch, Plus } from "phosphor-react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+import { HomePage } from "./pages/HomePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 export function App() {
-  const [openModalAddTransaction, setOpenModalAddTransaction] = useState(false);
-  const [transactions, setTransactios] = useState<Transaction[]>();
-  const [incomes, setIncomes] = useState<number>(0);
-  const [expenses, setExpenses] = useState<number>(0);
-  const [isLoading, setIsLoading] = useState(true);
-
-  function handleOpenModalAddTransaction() {
-    setOpenModalAddTransaction(true);
-  }
-
-  function handleCloseModalAddTransaction() {
-    setOpenModalAddTransaction(false);
-  }
-
-  useEffect(() => {
-    const unsub = onSnapshot(
-      doc(db, "users", "francissv97@gmail.com"),
-      (doc) => {
-        let incomeCount = 0;
-        let expenseCount = 0;
-
-        const data = doc.data() as { transactions: Transaction[] };
-
-        setTransactios(
-          data.transactions.sort(
-            (a: Transaction, b: Transaction) =>
-              a.date.toMillis() - b.date.toMillis()
-          )
-        );
-
-        for (let i in data.transactions) {
-          if (data.transactions[i].value >= 0) {
-            incomeCount += data.transactions[i].value / 100;
-          } else {
-            expenseCount += data.transactions[i].value / 100;
-          }
-        }
-
-        setIncomes(incomeCount);
-        setExpenses(expenseCount);
-        setIsLoading(false);
-      }
-    );
-
-    return unsub;
-  }, []);
-
   return (
-    <div className="flex flex-col bg-gradient-to-t from-slate-500 via-slate-300 to-slate-300 min-h-screen bg-no-repeat">
-      <Header />
-
-      <div className="max-w-xl flex mx-auto w-full flex-col gap-4 px-4 -translate-y-6">
-        <Numbers incomes={incomes} expenses={expenses} loading={isLoading} />
-
-        <button
-          onClick={handleOpenModalAddTransaction}
-          className="group flex items-center justify-center gap-2 bg-cyan-700 text-zinc-100 text-base rounded-md py-2 px-4 w-fit ml-auto transition hover:bg-cyan-800 hover:shadow-lg"
-        >
-          <Plus
-            size={32}
-            className="group duration-300 group-hover:rotate-90"
-          />
-          Nova Transação
-        </button>
-
-        <div className="h-[1px] my-4 bg-gradient-to-r from-slate-900 via-slate-500 to-slate-400 rounded-md"></div>
-
-        <div className="flex flex-col gap-4">
-          {isLoading ? (
-            <CircleNotch className="text-slate-500 animate-spin self-center" size={48} />
-          ) : (
-            transactions &&
-            transactions.map((transaction) => (
-              <TransactionCard key={transaction.id} transaction={transaction} />
-            ))
-          )}
-        </div>
-      </div>
-
-      <Footer className="mt-auto" />
-
-      {openModalAddTransaction && (
-        <ModalAddTransaction
-          open={openModalAddTransaction}
-          handleClose={handleCloseModalAddTransaction}
-        />
-      )}
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
