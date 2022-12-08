feee99fcac387b1de5d3c42a01b15d00b2cc9513
francissverissimo <francissv97@gmail.com>
2022-12-08 09:45:02 -0400

    add new routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
+import { ForgotPage } from "./pages/ForgotPage";
 import { HomePage } from "./pages/HomePage";
 import { NotFoundPage } from "./pages/NotFoundPage";
+import { SignUpPage } from "./pages/SignUpPage";
 
 export function App() {
   return (
@@ -9,6 +11,8 @@ export function App() {
       <AuthContextProvider>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/forgot" element={<ForgotPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </AuthContextProvider>
